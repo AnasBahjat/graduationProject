dab6ef4b3db78fc5766f872fd44843c317e9b96a
Asus <anasyasine22@gmail.com>
2024-01-23 17:13:35 +0200

    Creting Login screen with styled edit text for email and password 23/1/2023

--- app/src/main/java/com/example/graduationproject/LoginActivity.java
+++ app/src/main/java/com/example/graduationproject/LoginActivity.java
@@ -26,56 +26,53 @@ public class LoginActivity extends AppCompatActivity {
         email=findViewById(R.id.emailText);
         password=findViewById(R.id.passwordText);
         loginBtn=findViewById(R.id.loginBtn);
+    }
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+    public void loginClicked(View view) {
+        if(email.getEditText().getText().toString().isEmpty()){
+            email.setError("Fill in this field");
+        }
+        if(password.getEditText().getText().toString().isEmpty()){
+            password.setError("Fill in this field");
+        }
+
+        email.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                if(email.getEditText().getText().toString().isEmpty()){
-                    email.setError("Fill in this field");
-                }
-                if(password.getEditText().getText().toString().isEmpty()){
-                    password.setError("Fill in this field");
-                }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                email.getEditText().addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-                    }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                email.setError("");
+            }
 
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        email.setError("");
-                    }
+            @Override
+            public void afterTextChanged(Editable s) {
 
-                    @Override
-                    public void afterTextChanged(Editable s) {
-
-                    }
-                });
-
-
-                password.getEditText().addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        password.setError("");
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-
-                    }
-                });
-
-                if(!email.getEditText().getText().toString().isEmpty() && !password.getEditText().getText().toString().isEmpty()){
-                    Toast.makeText(LoginActivity.this,"Login done",Toast.LENGTH_SHORT).show();
-                }
             }
         });
+
+
+        password.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                password.setError("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        if(!email.getEditText().getText().toString().isEmpty() && !password.getEditText().getText().toString().isEmpty()){
+            Toast.makeText(LoginActivity.this,"Login done",Toast.LENGTH_SHORT).show();
+        }
     }
 }
