57ceeceed4b9bafdb117e8aaed2d7e229445fe11
naseembarghouthi97 <1151416@student.birzeit.edu>
2024-05-22 00:18:39 +0300

    Merge remote-tracking branch 'origin/master' i built these xml files initially fragment_student.xml selected_topic.xml //the previous two files for the student the main file is //fragment_student.xml and the second will used if any //topic choosed by the Student still need to programming : studnet.java StudentAdapters.java

---
+++ app/src/main/java/com/example/graduationproject/adapters/StudentAdapters.java
@@ -0,0 +1,4 @@
+package com.example.graduationproject.adapters;
+
+public class StudentAdapters {
+}
--- app/src/main/java/com/example/graduationproject/database/Database.java
+++ app/src/main/java/com/example/graduationproject/database/Database.java
@@ -30,12 +30,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 
 public class Database {
-    String registrationURL = "http://192.168.1.13/graduationProject/registration.php/";
-    String loginURL = "http://192.168.1.13/graduationProject/";
-    String updateLoginURL = "http://192.168.1.13/graduationProject/updateLoginState.php/";
-    String updateLogoutURL="http://192.168.1.13/graduationProject/updateLogoutState.php/";
+    String registrationURL = "http://192.168.1.136/graduationProject/registration.php/";
+    String loginURL = "http://192.168.1.136/graduationProject/";
+    String updateLoginURL = "http://192.168.1.136/graduationProject/updateLoginState.php/";
+    String updateLogoutURL="http://192.168.1.136/graduationProject/updateLogoutState.php/";
 
-    private String URL = "http://192.168.1.13/graduationProject/";
+    private String URL = "http://192.168.1.136/graduationProject/";
     private Context context;
     private RequestQueue requestQueue ;
     private int successFlag;
